fc43d2ed18210b0328e4d9a7e7718875758b1bf7
Kristof <kristof.kovacs@telenav.com>
2020-06-08 01:37:29 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ else:
 
 setuptools.setup(
     name="funcmeasure",
-    version="0.0.1",
+    version="0.0.2",
     author="Kristof",
     description="funcmeasure",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kkristof200/py_funcmeasure",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[""],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
